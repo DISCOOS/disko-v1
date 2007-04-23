fa4acfe7fb033d313b1dce04dde9f3ded6075d46
geir <geir@b0132108-b660-f846-a555-73bbf0b9793d>
2007-04-23 20:18:39 +0000

    Laget knapper i undermenypanelet og element dialog

--- org/redcross/sar/wp/DiskoWpTackticsImpl.java
+++ org/redcross/sar/wp/DiskoWpTackticsImpl.java
@@ -1,11 +1,12 @@
 package org.redcross.sar.wp;
 
 import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 
 import javax.swing.Icon;
 import javax.swing.JToggleButton;
-
 import org.redcross.sar.app.IDiskoApplication;
 import org.redcross.sar.app.IDiskoRole;
 import org.redcross.sar.app.Utils;
@@ -25,7 +26,16 @@ public class DiskoWpTackticsImpl extends AbstractDiskoWpModule implements IDisko
 
 	private IDiskoApplication app = null;
 	private FeatureLayer flankeFL = null;
+	private ElementDialog elementDialog = null;
 	private JToggleButton elementToggleButton = null;
+	private JToggleButton listToggleButton = null;
+	private JToggleButton missionToggleButton = null;
+	private JToggleButton hypotheseToggleButton = null;
+	private JToggleButton priorityToggleButton = null;
+	private JToggleButton requirementToggleButton = null;
+	private JToggleButton descriptionToggleButton = null;
+	private JToggleButton unitToggleButton = null;
+	
 	
 	/**
 	 * Constructs a DiskoApTaktikkImpl
@@ -41,7 +51,14 @@ public class DiskoWpTackticsImpl extends AbstractDiskoWpModule implements IDisko
 		loadProperties("properties");
 		DiskoMap map = getMap();
 		layoutComponent(map);
-		layoutButton(getElementToggleButton());
+		layoutButton(getElementToggleButton(), false);
+		layoutButton(getListToggleButton(), true);
+		layoutButton(getMissionToggleButton(), true);
+		layoutButton(getHypotheseToggleButton(), true);
+		layoutButton(getPriorityToggleButton(), true);
+		layoutButton(getRequirementToggleButton(), true);
+		layoutButton(getDescriptionToggleButton(), true);
+		layoutButton(getUnitToggleButton(), true);
 	}
 	
 	public void onMapReplaced(DiskoMapEvent e) throws IOException {
@@ -71,6 +88,10 @@ public class DiskoWpTackticsImpl extends AbstractDiskoWpModule implements IDisko
 		navBar.showButtons(buttonIndexes);
 	}
 	
+	public void deactivated() {
+		hideDialogs();
+    }
+	
 	/* (non-Javadoc)
 	 * @see com.geodata.engine.disko.task.DiskoAp#getName()
 	 */
@@ -82,12 +103,90 @@ public class DiskoWpTackticsImpl extends AbstractDiskoWpModule implements IDisko
 	 * @see com.geodata.engine.disko.task.DiskoAp#cancel()
 	 */
 	public void cancel() {
+		hideDialogs();
 	}
 	
 	/* (non-Javadoc)
 	 * @see com.geodata.engine.disko.task.DiskoAp#finish()
 	 */
 	public void finish() {
+		hideDialogs();
+	}
+	
+	private void hideDialogs() {
+		getElementDialog().setVisible(false);
+		getElementToggleButton().setSelected(false);
+	}
+	
+	private ElementDialog getElementDialog() {
+		if (elementDialog == null) {
+			elementDialog = new ElementDialog(app.getFrame(), new ActionListener() {
+				public void actionPerformed(java.awt.event.ActionEvent e) {
+					getElementDialog().setVisible(false);
+					getElementToggleButton().setSelected(false);
+					String command = e.getActionCommand();
+					if (command.equals("ELEMENT_OPERATION_AREA")) {
+						showOperationAreaButtons();
+					}
+					else if (command.equals("ELEMENT_SEARCH_AREA")) {
+						showSearchAreaButtons();
+					}
+					else if (command.equals("ELEMENT_PATROL_SEARCH")) {
+						showSearchButtons();
+					}
+					else if (command.equals("ELEMENT_LINE_SEARCH")) {
+						showSearchButtons();
+					}
+					else if (command.equals("ELEMENT_RODE_SEARCH")) {
+						showSearchButtons();
+					}
+					else if (command.equals("ELEMENT_BEACH_SEARCH")) {
+						showSearchButtons();
+					}
+					else if (command.equals("ELEMENT_MARITIME_SEARCH")) {
+						showSearchButtons();
+					}
+					else if (command.equals("ELEMENT_DOG_SEARCH")) {
+						showSearchButtons();
+					}
+					else if (command.equals("ELEMENT_AIR_SEARCH")) {
+						showSearchButtons();
+					}
+					else if (command.equals("ELEMENT_GENERAL_TASK")) {
+						showSearchButtons();
+					}
+				}
+			});
+			elementDialog.setIsToggable(false);
+		}
+		return elementDialog;
+	}
+	
+	private void showOperationAreaButtons() {
+		getMissionToggleButton().setVisible(true);
+		getHypotheseToggleButton().setVisible(false);
+		getPriorityToggleButton().setVisible(false);
+		getRequirementToggleButton().setVisible(false);
+		getDescriptionToggleButton().setVisible(false);
+		getUnitToggleButton().setVisible(false);
+	}
+	
+	private void showSearchAreaButtons() {
+		getMissionToggleButton().setVisible(false);
+		getHypotheseToggleButton().setVisible(true);
+		getPriorityToggleButton().setVisible(true);
+		getRequirementToggleButton().setVisible(false);
+		getDescriptionToggleButton().setVisible(false);
+		getUnitToggleButton().setVisible(false);
+	}
+	
+	private void showSearchButtons() {
+		getMissionToggleButton().setVisible(false);
+		getHypotheseToggleButton().setVisible(false);
+		getPriorityToggleButton().setVisible(false);
+		getRequirementToggleButton().setVisible(true);
+		getDescriptionToggleButton().setVisible(true);
+		getUnitToggleButton().setVisible(true);
 	}
 
 	private JToggleButton getElementToggleButton() {
@@ -100,11 +199,186 @@ public class DiskoWpTackticsImpl extends AbstractDiskoWpModule implements IDisko
 				elementToggleButton.setText("ELEMENT");
 				//elementToggleButton.setIcon(icon);
 				elementToggleButton.setPreferredSize(size);
+				elementToggleButton.addActionListener(new java.awt.event.ActionListener() {
+					public void actionPerformed(java.awt.event.ActionEvent e) {
+						ElementDialog dialog = getElementDialog();
+						if (elementToggleButton.isSelected()) {
+							Point p = elementToggleButton.getLocationOnScreen();
+							p.setLocation(p.x - dialog.getWidth()-2, p.y);
+							dialog.setLocation(p);
+							dialog.setVisible(true);
+						}
+						else {
+							dialog.setVisible(false);
+						}
+					}
+				});
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
 		return elementToggleButton;
+	}
+
+	private JToggleButton getDescriptionToggleButton() {
+		if (descriptionToggleButton == null) {
+			try {
+				Dimension size = app.getUIFactory().getLargeButtonSize();
+				//String iconName = "MapZoomInTool.icon";
+				//Icon icon = Utils.createImageIcon(app.getProperty(iconName),iconName);
+				descriptionToggleButton = new JToggleButton();
+				descriptionToggleButton.setText("BESKRIVELSE");
+				//descriptionToggleButton.setIcon(icon);
+				descriptionToggleButton.setPreferredSize(size);
+				descriptionToggleButton.addActionListener(new java.awt.event.ActionListener() {
+					public void actionPerformed(java.awt.event.ActionEvent e) {
+						hideDialogs();
+					}
+				});
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return descriptionToggleButton;
+	}
+
+	private JToggleButton getHypotheseToggleButton() {
+		if (hypotheseToggleButton == null) {
+			try {
+				Dimension size = app.getUIFactory().getLargeButtonSize();
+				String iconName = "hypothese.icon";
+				Icon icon = Utils.createImageIcon(app.getProperty(iconName),iconName);
+				hypotheseToggleButton = new JToggleButton();
+				hypotheseToggleButton.setIcon(icon);
+				hypotheseToggleButton.setPreferredSize(size);
+				hypotheseToggleButton.setVisible(false);
+				hypotheseToggleButton.addActionListener(new java.awt.event.ActionListener() {
+					public void actionPerformed(java.awt.event.ActionEvent e) {
+						hideDialogs();
+					}
+				});
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return hypotheseToggleButton;
+	}
+
+	private JToggleButton getListToggleButton() {
+		if (listToggleButton == null) {
+			try {
+				Dimension size = app.getUIFactory().getLargeButtonSize();
+				String iconName = "list.icon";
+				Icon icon = Utils.createImageIcon(app.getProperty(iconName),iconName);
+				listToggleButton = new JToggleButton();
+				listToggleButton.setIcon(icon);
+				listToggleButton.setPreferredSize(size);
+				listToggleButton.addActionListener(new java.awt.event.ActionListener() {
+					public void actionPerformed(java.awt.event.ActionEvent e) {
+						hideDialogs();
+					}
+				});
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return listToggleButton;
+	}
+
+	private JToggleButton getMissionToggleButton() {
+		if (missionToggleButton == null) {
+			try {
+				Dimension size = app.getUIFactory().getLargeButtonSize();
+				String iconName = "mission.icon";
+				Icon icon = Utils.createImageIcon(app.getProperty(iconName),iconName);
+				missionToggleButton = new JToggleButton();
+				missionToggleButton.setIcon(icon);
+				missionToggleButton.setPreferredSize(size);
+				missionToggleButton.setVisible(false);
+				missionToggleButton.addActionListener(new java.awt.event.ActionListener() {
+					public void actionPerformed(java.awt.event.ActionEvent e) {
+						hideDialogs();
+					}
+				});
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return missionToggleButton;
+	}
+
+	private JToggleButton getPriorityToggleButton() {
+		if (priorityToggleButton == null) {
+			try {
+				Dimension size = app.getUIFactory().getLargeButtonSize();
+				//String iconName = "MapZoomInTool.icon";
+				//Icon icon = Utils.createImageIcon(app.getProperty(iconName),iconName);
+				priorityToggleButton = new JToggleButton();
+				priorityToggleButton.setText("PRIORITET");
+				//priorityToggleButton.setIcon(icon);
+				priorityToggleButton.setPreferredSize(size);
+				priorityToggleButton.setVisible(false);
+				priorityToggleButton.addActionListener(new java.awt.event.ActionListener() {
+					public void actionPerformed(java.awt.event.ActionEvent e) {
+						hideDialogs();
+					}
+				});
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return priorityToggleButton;
+	}
+
+	private JToggleButton getRequirementToggleButton() {
+		if (requirementToggleButton == null) {
+			try {
+				Dimension size = app.getUIFactory().getLargeButtonSize();
+				//String iconName = "MapZoomInTool.icon";
+				//Icon icon = Utils.createImageIcon(app.getProperty(iconName),iconName);
+				requirementToggleButton = new JToggleButton();
+				requirementToggleButton.setText("KRAV");
+				//requirementToggleButton.setIcon(icon);
+				requirementToggleButton.setPreferredSize(size);
+				requirementToggleButton.addActionListener(new java.awt.event.ActionListener() {
+					public void actionPerformed(java.awt.event.ActionEvent e) {
+						hideDialogs();
+					}
+				});
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return requirementToggleButton;
+	}
+
+	private JToggleButton getUnitToggleButton() {
+		if (unitToggleButton == null) {
+			try {
+				Dimension size = app.getUIFactory().getLargeButtonSize();
+				//String iconName = "MapZoomInTool.icon";
+				//Icon icon = Utils.createImageIcon(app.getProperty(iconName),iconName);
+				unitToggleButton = new JToggleButton();
+				unitToggleButton.setText("ENHET");
+				//unitToggleButton.setIcon(icon);
+				unitToggleButton.setPreferredSize(size);
+				unitToggleButton.addActionListener(new java.awt.event.ActionListener() {
+					public void actionPerformed(java.awt.event.ActionEvent e) {
+						hideDialogs();
+					}
+				});
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return unitToggleButton;
 	}
 }
