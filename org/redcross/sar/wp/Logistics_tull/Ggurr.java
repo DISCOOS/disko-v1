3a5e757c920a27f0b83e126c217ff3d8a8b4d0cc
vinjar <vinjar@b0132108-b660-f846-a555-73bbf0b9793d>
2007-05-25 12:13:22 +0000

    Test på å rydd opp i gammelt rusk i svn

---
+++ org/redcross/sar/wp/Logistics_tull/Ggurr.java
@@ -0,0 +1,14 @@
+package org.redcross.sar.wp.Logistics;
+/**
+ * Created by IntelliJ IDEA.
+ * User: vinjar
+ * Date: 25.mai.2007
+ * To change this template use File | Settings | File Templates.
+ */
+
+/**
+ *
+ */
+public class Ggurr
+{
+}
