4a1f54c8bb5ea0567ebeccf94573799e8ebc0feb
vinjar <vinjar@b0132108-b660-f846-a555-73bbf0b9793d>
2007-06-21 10:22:46 +0000

    Improved

--- org/redcross/sar/gui/DiskoBorder.java
+++ org/redcross/sar/gui/DiskoBorder.java
@@ -1,16 +1,10 @@
 package org.redcross.sar.gui;
 
-import java.awt.BorderLayout;
-import java.awt.Component;
-import java.awt.Graphics;
-import java.awt.Insets;
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.border.Border;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,51 +15,82 @@ import javax.swing.border.Border;
  */
 public class DiskoBorder implements Border
 {
+    private int radius;
+    private int thickness;
 
-   private int radius;
+    DiskoBorder(int aRadius, int aThickness)
+    {
+        radius = aRadius;
+        thickness = aThickness;
+    }
 
-   DiskoBorder(int radius)
-   {
-      this.radius = radius;
-   }
-
-   public Insets getBorderInsets(Component c)
-   {
-      return new Insets(this.radius + 1, this.radius + 1, this.radius + 2, this.radius);
-   }
-
-
-   public boolean isBorderOpaque()
-   {
-      return true;
-   }
+    public Insets getBorderInsets(Component c)
+    {
+        return new Insets(radius*2 + thickness * 2,radius*2 + thickness * 2, radius + thickness*2,  radius + thickness*2);
+    }
 
 
-   public void paintBorder(Component c, Graphics g, int x, int y, int width, int height)
-   {
-      g.drawRoundRect(x + 1, y, width - 2, height - 1, radius, radius);
-      g.drawRoundRect(x + 1, y + radius, width - 2, height - 1 - radius, radius, radius);
-   }
+    public boolean isBorderOpaque()
+    {
+        return true;
+    }
 
-   public static void main(String args[])
-   {
-      JFrame fr = new JFrame();
-      fr.setSize(400, 500);
-      JLabel label = new JLabel("TEsting");
-      label.setBorder(new DiskoBorder(5));
-      JButton butt = new JButton("Test butt");
-      butt.setBorder(new DiskoBorder(5));
-      fr.getContentPane().add(label, BorderLayout.NORTH);
-      fr.getContentPane().add(butt, BorderLayout.SOUTH);
-      fr.setVisible(true);
-      fr.addWindowListener(new WindowAdapter()
-      {
 
-         public void windowClosed(WindowEvent e)
-         {
-            System.exit(0);
-         }
-      });
-   }
+    public void paintBorder(Component c, Graphics g, int x, int y, int width, int height)
+    {
+        System.out.println(x + " " + y + " " + width + " " + height);
+        Graphics2D g2 = (Graphics2D) g;
+        Stroke oldStroke = g2.getStroke();
+        Stroke linestroke = new BasicStroke(thickness);
+//        Stroke widestroke = new BasicStroke(radius);
+
+        int vertDispl = radius + thickness;
+        int horDispl = radius / 2 + thickness;
+
+        g2.setStroke(linestroke);
+        g.drawRoundRect(x + thickness / 2,
+                y + thickness / 2,
+                width - 1 - radius - thickness,
+                height - 2 - radius - thickness,
+                radius, radius);
+
+        g.setColor(c.getBackground());
+//        g.setColor(Color.red);
+        g.drawRoundRect(x + thickness / 2 + horDispl,
+                y + thickness / 2 + vertDispl,
+                width - 1 - radius  - thickness- horDispl,
+                height - 2 - radius  - thickness- vertDispl,
+                radius, radius);
+
+        g.setColor(c.getForeground());
+        g2.setStroke(linestroke);
+        g.drawRoundRect(x + thickness / 2 + horDispl,
+                y + thickness / 2 + vertDispl,
+                width - 1 - radius - thickness,
+                height - 2 - radius - thickness,
+                radius, radius);
+        g2.setStroke(oldStroke);
+    }
+
+    public static void main(String args[])
+    {
+        JFrame fr = new JFrame();
+        fr.setSize(400, 500);
+        JLabel label = new JLabel("Testing");
+        label.setBorder(new DiskoBorder(5, 2));
+        JButton butt = new JButton("Test butt");
+        butt.setBorder(new DiskoBorder(5,2));
+        fr.getContentPane().add(label, BorderLayout.NORTH);
+        fr.getContentPane().add(butt, BorderLayout.SOUTH);
+        fr.setVisible(true);
+        fr.addWindowListener(new WindowAdapter()
+        {
+
+            public void windowClosed(WindowEvent e)
+            {
+                System.exit(0);
+            }
+        });
+    }
 
 }
