c63a02311812482af765d380b7adfd721f1d5838
vinjar <vinjar@b0132108-b660-f846-a555-73bbf0b9793d>
2007-06-26 12:40:42 +0000

    Working on MessageLog

--- org/redcross/sar/wp/TestData/BuildTestData.java
+++ org/redcross/sar/wp/TestData/BuildTestData.java
@@ -5,6 +5,8 @@ import org.redcross.sar.mso.IMsoModelIf;
 import org.redcross.sar.mso.data.*;
 import org.redcross.sar.util.except.DuplicateIdException;
 import org.redcross.sar.util.except.MsoException;
+
+import java.util.Calendar;
 /**
  * Created by IntelliJ IDEA.
  * User: vinjar
@@ -15,13 +17,17 @@ import org.redcross.sar.util.except.MsoException;
 /**
  *
  */
-public class BuildTestData {
-    public static void createCmdPost(IMsoModelIf aMsoModel) {
+public class BuildTestData
+{
+    public static void createCmdPost(IMsoModelIf aMsoModel)
+    {
         aMsoModel.setLocalUpdateMode();
         IMsoManagerIf msoManager = aMsoModel.getMsoManager();
         IOperationIf testOperation = msoManager.getOperation();
-        if (testOperation == null) {
-            try {
+        if (testOperation == null)
+        {
+            try
+            {
                 testOperation = msoManager.createOperation("2007-TEST", "0001");
             }
             catch (DuplicateIdException e) // shall not happen
@@ -30,8 +36,10 @@ public class BuildTestData {
             }
         }
         ICmdPostIf cmdPost = msoManager.getCmdPost();
-        if (cmdPost == null) {
-            try {
+        if (cmdPost == null)
+        {
+            try
+            {
                 cmdPost = msoManager.createCmdPost();
             }
             catch (DuplicateIdException e) // shall not happen
@@ -39,14 +47,16 @@ public class BuildTestData {
                 e.printStackTrace();
             }
         }
-        if (cmdPost != null) {
+        if (cmdPost != null)
+        {
             cmdPost.setStatus(ICmdPostIf.CmdPostStatus.OPERATING);
         }
-        //aMsoModel.restoreUpdateMode();
+        aMsoModel.restoreUpdateMode();
         aMsoModel.commit();
     }
 
-    public static void createUnitsAndAssignments(IMsoModelIf aMsoModel) {
+    public static void createUnitsAndAssignments(IMsoModelIf aMsoModel)
+    {
         ICmdPostIf cmdPost = aMsoModel.getMsoManager().getCmdPost();
         aMsoModel.setLocalUpdateMode();
         IUnitListIf unitList = cmdPost.getUnitList();
@@ -75,13 +85,15 @@ public class BuildTestData {
         unit.setStatus(IUnitIf.UnitStatus.EMPTY);
 
         IAssignmentListIf asgList = cmdPost.getAssignmentList();
-        try {
+        try
+        {
             IAssignmentIf asg;
             IAssignmentIf prevAsg = null;
             boolean insertionToggle = false;
 
             unit = unitList.getUnit(1);
-            for (int i = 0; i < 10; i++) {
+            for (int i = 0; i < 10; i++)
+            {
                 asg = asgList.createSearch();
                 asg.setStatusAndOwner(IAssignmentIf.AssignmentStatus.DRAFT, null);
                 asg.setStatusAndOwner(IAssignmentIf.AssignmentStatus.READY, null);
@@ -92,7 +104,8 @@ public class BuildTestData {
             }
 
             unit = unitList.getUnit(2);
-            for (int i = 0; i < 8; i++) {
+            for (int i = 0; i < 8; i++)
+            {
                 asg = asgList.createSearch();
                 asg.setStatusAndOwner(IAssignmentIf.AssignmentStatus.DRAFT, null);
                 asg.setStatusAndOwner(IAssignmentIf.AssignmentStatus.READY, null);
@@ -102,12 +115,14 @@ public class BuildTestData {
                 insertionToggle = !insertionToggle;
             }
 
-            for (int i = 0; i < 20; i++) {
+            for (int i = 0; i < 20; i++)
+            {
                 asg = asgList.createSearch();
                 asg.setStatusAndOwner(IAssignmentIf.AssignmentStatus.DRAFT, null);
                 asg.setStatusAndOwner(IAssignmentIf.AssignmentStatus.READY, null);
                 ((ISearchIf) asg).setSubType(ISearchIf.SearchSubType.PATROL);
-                switch (i % 3) {
+                switch (i % 3)
+                {
                     case 0:
                         asg.setPriority(IAssignmentIf.AssignmentPriority.LOW);
                         break;
@@ -122,11 +137,32 @@ public class BuildTestData {
             }
 
         }
-        catch (MsoException e) {
+        catch (MsoException e)
+        {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
 
-        //aMsoModel.restoreUpdateMode();
+        aMsoModel.restoreUpdateMode();
+        aMsoModel.commit();
+    }
+
+    public static void createMessages(IMsoModelIf aMsoModel)
+    {
+        ICmdPostIf cmdPost = aMsoModel.getMsoManager().getCmdPost();
+        aMsoModel.setLocalUpdateMode();
+
+        IMessageLogIf messageLog = cmdPost.getMessageLog();
+        IMessageIf message;
+        IMessageLineIf messageLine;
+
+        message = messageLog.createMessage();
+        message.setCreated(Calendar.getInstance());
+        message.setCalendar(Calendar.getInstance());
+        message.setStatus(IMessageIf.MessageStatus.UNCONFIRMED);
+        messageLine = message.findMessageLine(IMessageLineIf.MessageLineType.TEXT,true);
+        messageLine.setText("Tekst Linje 1");
+
+        aMsoModel.restoreUpdateMode();
         aMsoModel.commit();
     }
 
